e9ae270b3421bedf2d3b5b9930cbb030596db467
Alistair Willis <alistairrwillis@gmail.com>
2017-01-11 16:55:51 +0900

    Added function to convert date to unix timestamp. Added hbs view.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const hbs = require('handlebars'); 
 
-const app = new express(); 
+var app = new express(); 
 
 const port = process.env.PORT || 3000; 
 
+app.set('view engine', hbs); 
+
 app.get('/', (req, res) => {
-    res.send('hello'); 
+    res.render(__dirname + '/public/views/index.hbs');
+});
+
+app.get('/date', (req, res) => {
+    var dateSent = req.query.date; 
+var timestamp = parseInt((new Date(dateSent).getTime()) / 1000); 
+    res.render(__dirname + '/public/views/index.hbs', {
+        date: dateSent, 
+        timestamp: timestamp
+    });
+    console.log(req.query.date);
+    
 });
 
 app.listen(port, () => {
